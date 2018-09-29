f5a8ba73b206b8a684b900cb995bd49ed717980e
Kasia Kłosowska <kasiaklosowska21@gmail.com>
2018-09-29 16:48:31 +0200

    Zadanie nr 5

--- js/app.js
+++ js/app.js
@@ -30,7 +30,39 @@ menuButton.addEventListener('mouseout', function() {
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', showHide);
 }
+    var menu = document.querySelector('.navbar');
+    window.addEventListener('scroll', function() {
+      console.log(menu.style.borderBottom = '1px solid #7f7f7f');
+    });
+    var goBack = document.querySelector('.btn-back');
+    goBack.addEventListener('click', function() {
+        window.scroll(0,0);
+    });
     
+    var items = document.querySelectorAll('.org');
+    var dots = document.querySelectorAll('.dot');
+    var indexClicked;
+    for(var i = 0; i < dots.length; i++) {
+        dots[i].addEventListener('click', function() {
+            for(var i = 0; i < items.length; i++) {
+    dots[i].classList.remove('active');
+    items[i].classList.remove('visible');
+            }
+
+this.classList.add('active');   
+            
+    var clickedDot = document.getElementsByClassName('active');
+            console.log(clickedDot);
+            
+            for (var i = 0; i < dots.length; i++) {
+                if (dots[i].classList.contains('active')) {
+                indexClicked = i;
+                    break;
+                }
+            }
+        items[indexClicked].classList.add('visible');
+    });
+    }
 });
 
 
